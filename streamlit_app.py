3291e78129c901507292ed7886e1b28fa41c0839
nada-kamal-sketch <nhega854@gmail.com>
2024-08-27 23:43:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ Age=st.number_input('feature_3',min_value=0,max_value=100)
 with open('model.pkl', 'rb') as file:
     model = pickle.load(file)
     
-output = model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+
 
 
