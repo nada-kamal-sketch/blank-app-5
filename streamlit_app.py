2b799c0db2d62ff7580aa8874629186c61df9183
nada-kamal-sketch <nhega854@gmail.com>
2024-08-27 23:41:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ with open('model.pkl', 'rb') as file:
     
 output = model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
 
-st.write("the predict is : ",output[0])
+st.write("the predict is : ",output[0][0])
 
